bfb3fc8f52d9d22961f8373c94dc583f8e088524
Kurtis McCammon <maxgamer898@gmail.com>
2022-04-29 10:07:34 -0500

    Closing now actually ends the process.
    Don't know why I waited so long to fix this issue.

--- PythonRender.py
+++ PythonRender.py
@@ -2,23 +2,28 @@ import tkinter as tk
 from Math3D import RotateX, RotateY, Vec4
 import VoidPython as VP
 
-
 class application(tk.Frame):
+
+    windowOpen = True
+
     def __init__(self, master=None) -> None:
         super().__init__(master)
         self.canvas = tk.Canvas(width=200, height=200)
 
         self.canvas.pack()
 
+    def onWindowClose(self):
+        self.windowOpen = False
 
 def Main():
     root = tk.Tk()
     primApp = application(root)
+    root.protocol("WM_DELETE_WINDOW", primApp.onWindowClose)
     primeScene = VP.Scene()
-    primeScene.Camera = VP.Camera(position=Vec4(100,100,100))
+    primeScene.Camera = VP.Camera(position=Vec4(0,0,-5))
     primeScene.gameObjects.clear()
     cubeObj = VP.gameObject()
-    cubeObj.myMesh = VP.CreateCube(0.5)
+    cubeObj.myMesh = VP.CreateCube(1)
     RotateX(cubeObj.transform, 45.0)
     primeScene.addObject(cubeObj)
 
@@ -28,10 +33,10 @@ def Main():
         for obj in primeScene.gameObjects:
            currPoints = primeScene.gameObjects[0].myMesh.ProjectedPoints
            for set in obj.myMesh.sets:
-               primApp.canvas.create_line(currPoints[set[0]].x(), currPoints[set[0]].y(),
-                                          currPoints[set[1]].x(), currPoints[set[1]].y(),
-                                          currPoints[set[2]].x(), currPoints[set[2]].y(),
-                                          currPoints[set[0]].x(), currPoints[set[0]].y(),)
+               primApp.canvas.create_line(currPoints[set[0]].x()*200, currPoints[set[0]].y()*200,
+                                          currPoints[set[1]].x()*200, currPoints[set[1]].y()*200,
+                                          currPoints[set[2]].x()*200, currPoints[set[2]].y()*200,
+                                          currPoints[set[0]].x()*200, currPoints[set[0]].y()*200,)
 
     if False:
         for x in range(0, 1):
@@ -41,9 +46,11 @@ def Main():
                                        currPoints[currSet[1]].x()+50, currPoints[currSet[1]].y()+50,
                                        currPoints[currSet[2]].x()+50, currPoints[currSet[2]].y()+50,
                                        currPoints[currSet[0]].x()+50, currPoints[currSet[0]].y()+50,)
-    while True:
+    while primApp.windowOpen:
         primApp.update_idletasks()
         primApp.update()
+
+    primApp.master.destroy()
 
 
 if __name__ == '__main__':
